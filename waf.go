686fabe4ef93240f8aaa699554c090eeda17fd02
hmaima <137321954+hmaima@users.noreply.github.com>
2023-08-02 22:04:41 +0100

    waf.go

---
+++ waf.go
@@ -0,0 +1,65 @@
+package waf
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/kitabisa/teler-waf"
+	"github.com/kitabisa/teler-waf/request"
+	"github.com/kitabisa/teler-waf/threat"
+	// /"github.com/govwa/user/session"
+)
+
+// convert httprouter.Handle to http.HandlerFunc
+func ConvertToHandlerFunc(h httprouter.Handle) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		params := httprouter.ParamsFromContext(r.Context())
+		h(w, r, params)
+	}
+}
+
+// Convert http.Handler to httprouter.Handle
+func ConvertToHttprouterHandle(h http.Handler) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+		h.ServeHTTP(w, r)
+	}
+}
+
+var TelerWAF = teler.New(teler.Options{
+	Excludes: []threat.Threat{
+		threat.BadReferrer,
+		threat.BadCrawler,
+	},
+	Whitelists: []string{
+		`request.Headers matches "(curl|Go-http-client|okhttp)/*"`,
+		`request.IP in ["127.0.0.1", "::1", "0.0.0.0"]`,
+	},
+	CustomsFromFile: "",
+	Customs: []teler.Rule{
+		{
+			Name:      "SQL Injection",
+			Condition: "or",
+			Rules: []teler.Condition{
+				{
+					Method:  request.ALL,
+					Element: request.Any,
+					Pattern: `(?i)\b(?:select|union|insert|update|delete|drop|exec)\b`,
+				},
+			},
+		},
+	},
+
+	LogFile: "teler.log",
+})
+
+var rejectHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// This is the handler function for the route that we want to be rejected
+	// if the teler-waf's security measures are triggered.
+	http.Error(w, "Sorry, your request has been denied for security reasons.", http.StatusForbidden)
+})
+
+func main() {
+
+	TelerWAF.SetHandler(rejectHandler)
+
+}
