6192492ced59beb7062709e1149e4a03041ae312
hmaima <137321954+hmaima@users.noreply.github.com>
2023-08-25 12:16:41 +0100

    Update waf.go

--- waf.go
+++ waf.go
@@ -66,7 +66,7 @@ var TelerWAF = teler.New(teler.Options{
 				},
 			},
 		},
-		{
+		/*{
 			Name:      "finds html breaking injections including whitespace attacks",
 			Condition: "or",
 			Rules: []teler.Condition{
@@ -88,7 +88,7 @@ var TelerWAF = teler.New(teler.Options{
 					Pattern: `(?:\"+.*[<=]\\s*\"[^\"]+\")|(?:\"\\s*\\w+\\s*=)|(?:>\\w=\\/)|(?:#.+\\)[\"\\s]*>)|(?:\"\\s*(?:src|style|on\\w+)\\s*=\\s*\")|(?:[^\\"+]?\"[,;\\s]+\\w*[\\[\\(])`,
 				},
 			},
-		},
+		},*/
 
 		{
 			Name:      "finds unquoted attribute breaking injections",
